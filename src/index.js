c2149c917a5a2217e2f93316d2e83441f3298764
tudorneagu <neagu.t@gmail.com>
2024-08-13 13:08:01 +0200

    retrive movie API call - ok

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
 
 import { useState } from "react";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating color="blue" maxRating={"sss"} onSetRating={setMovieRating} />
-      <p> This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={"sss"} onSetRating={setMovieRating} />
+//       <p> This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App />
-     */}
-    <StarRating maxRating={10} defaultRating={3} />
+    <App />
+
+    {/* <StarRating maxRating={10} defaultRating={3} />
     <StarRating
       size={24}
       color="red"
@@ -29,6 +29,6 @@ root.render(
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
       defaultRating={3}
     />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
